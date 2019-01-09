7feb77a6f73ca9ef3a1a4af22265ac0ab04a0616
Bianca Del Carretto <bianca.delcarretto@wyscout.com>
2019-01-09 17:10:55 +0100

    middlewares finiti

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reduxPromise from 'redux-promise'
 
 import reducers from 'reducers'
+import async from 'middlewares/async'
+import stateValidator from 'middlewares/stateValidator'
 
 const Root = ({ children, initialState }) => {
     const store = createStore(
         reducers,
         initialState,
-        applyMiddleware(reduxPromise)
+        applyMiddleware(async, stateValidator)
     )
     return <Provider store={store}>{children}</Provider>
 }
